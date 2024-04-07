f85200078dedd8fe57e1261b4fc7c15e30a0ed98
ipsita <ipsitapurohit0@gmail.com>
2024-04-07 10:20:03 -0700

    updated fetch code with monthly data, added pretty print code and added updated data files

--- Code_snippets/nflx_fetch.py
+++ Code_snippets/nflx_fetch.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "https://apistocks.p.rapidapi.com/daily"
+url = "https://apistocks.p.rapidapi.com/monthly"
 
-querystring = {"symbol":"NFLX","dateStart":"2000-01-01","dateEnd":"2023-12-31"}
+querystring = {"symbol":"NFLX","dateStart":"2000-01-01","dateEnd":"2024-01-01"}
 
 headers = {
 	"X-RapidAPI-Key": "aba5f18006mshf50a0e95138d6f9p1f5aefjsn99f18a46e2be",
